40f545b13330463678db79b807dc1db855d514b9
willtaylor072 <taylorwill332@gmail.com>
2024-11-18 16:57:04 +0000

    .

--- main.py
+++ main.py
@@ -102,27 +102,82 @@ def FT(x):
 def IFT(x):
     return np.fft.fftshift(ifft2(np.fft.ifftshift(x)))
 
+# Update kx and ky for the current image by finding the kx and ky that minimise error between image estimate
+# and current low res image. search_range should be odd
+def update_LED_positions_accurate(obj,img,kx,ky,img_size,obj_center,image_number,search_range=15):
+    
+    # Easier to crop obj using x_start and y_start 
+    x_start = int(obj_center + kx - img_size//2)
+    y_start = int(obj_center - ky - img_size//2)  
+    
+    # Define the range we will search for a minimum
+    x_offsets = range(-(search_range // 2), (search_range // 2) + 1)
+    y_offsets = range(-(search_range // 2), (search_range // 2) + 1)
+
+    min_error = float('inf') # Min error for this iteration
+    # error_heatmap = np.zeros((search_range,search_range)) # For visualising algorithm
+    
+    # Find error between image and estimated image, where we offset the object crop region slightly to find estimated image
+    for x in x_offsets:
+        for y in y_offsets:       
+            img_est = IFT(obj[y_start+y:y_start+y+img_size, x_start+x:x_start+x+img_size]) # Estimated image is IFT of cropped spectrum at the shifted center
+            error = np.sum(np.abs(img_est - img)**2) # Error between estimated and measured image
+            # error_heatmap[(search_range // 2 - y), (x + search_range // 2)] = error # Add error to heatmap (convert from cartesian to image coords)
+    
+            # Track the smallest error position
+            if error < min_error:
+                min_error = error
+                optimal_x = x # Offsets for mimimum error within the sub-region
+                optimal_y = y
+            
+    # # Plot the heatmap for a specific image number
+    # if image_number == 15:  # Image number to inspect correction algorithm
+    #     plt.imshow(error_heatmap, cmap='hot', extent=[x_offsets.start, x_offsets.stop - 1, y_offsets.start, y_offsets.stop - 1])
+    #     plt.colorbar(label='Error')
+    #     plt.xlabel('X Offset')
+    #     plt.ylabel('Y Offset')
+    #     plt.title(f'Error Landscape, image {image_number}')
+
+    #     # Label the minimum error cell
+    #     plt.text(optimal_x, optimal_y, 'X', color='white', fontsize=12, ha='center', va='center', fontweight='bold')
+    #     plt.show()
+        
+    #     # Diagnostics
+    #     print(f"optimal_x: {optimal_x}, optimal_y: {optimal_y}, min_error: {min_error}, x_start: {x_start}, y_start: {y_start}")
+
+                    
+    # We have optimal x_start and y_start so just rearrange for kx,ky using below relations
+    # x_start = int(obj_center + kx[i] - img_size//2)
+    # y_start = int(obj_center - ky[i] - img_size//2)  
+    kx = x_start - obj_center + img_size//2
+    ky = obj_center - img_size//2 - y_start
+        
+    return kx,ky 
+
 # Plotting for visualising reconstruction (used in reconstruct function)
-def plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter):
+def plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter,plot_mode,update_size):
     # Clear previous plots
     for ax in axes:
         ax.cla()  # Clear the current axes
       
     axes[0].imshow(np.log(np.abs(obj) + 1), cmap='gray') # Show with log scale
     axes[0].set_title(f'Spectrum of object: image {i+1}, iteration {iter+1} ')
-    square = patches.Rectangle((x_start, y_start), img_size, img_size, linewidth=0.5, edgecolor='red', facecolor='none')
-    axes[0].add_patch(square)
-    circle = patches.Circle((obj_center+kx[i],obj_center-ky[i]), radius=pupil_radius,linewidth =0.5, edgecolor='red',facecolor='none')
-    axes[0].add_patch(circle)
+    if plot_mode == 1:
+        square = patches.Rectangle((x_start, y_start), img_size, img_size, linewidth=0.5, edgecolor='red', facecolor='none')
+        axes[0].add_patch(square)
+        circle = patches.Circle((obj_center+kx[i],obj_center-ky[i]), radius=pupil_radius,linewidth =0.5, edgecolor='red',facecolor='none')
+        axes[0].add_patch(circle)
 
     axes[1].imshow(np.abs(IFT(obj)), cmap='gray')
     axes[1].set_title('Currently reconstructed object')
     
-    axes[2].imshow(np.angle(pupil),cmap='gray')
-    axes[2].set_title('Current pupil phase')
-    
-    axes[3].imshow(np.abs(pupil),cmap='gray')
-    axes[3].set_title('Current pupil magnitude')
+    # axes[2].imshow(np.angle(pupil),cmap='gray')
+    # axes[2].set_title('Current pupil phase')
+    # axes[2].imshow(np.abs(pupil),cmap='gray')
+    # axes[2].set_title('Current pupil magnitude')
+    axes[2].plot(update_size)
+    axes[2].set_title('Object update size')
+    axes[2].set_ylim(0,0.5)
     
     # Update the figure
     plt.tight_layout()  # Optional: adjusts subplot params for a nicer layout
@@ -136,40 +191,69 @@ def reconstruct(images, kx, ky, obj, pupil, options):
     # kx,ky; location of LEDs in Fourier domain, in order of images taken
     # obj; initial estimate for object in frequency domain
     # pupil; initial pupil function
-    # options; alpha, beta (regularisation), max_iter, plotting
+    # options; alpha, beta (regularisation), max_iter, plotting, quality_threshold
     
     # Returns: 
     # rec_obj; recovered object
     # rec_pupil; recovered pupil function
+    # kx, ky; updated LED positions
     
     # Unpack options
-    alpha = options['alpha']
-    beta = options['beta']
-    max_iter = options['max_iter']
+    alpha = options['alpha'] # <10, DOES MAKE DIFFERENCE
+    beta = options['beta'] # Not important
+    max_iter = options['max_iter'] # Number of iterations to run algorithm (1 iteration uses all images)
     plot_mode = options['plot_mode'] # 0, off; 1, plot every image; 2, plot every iteration
+    quality_threshold = options['quality_threshold'] # Used to crop bad images from dataset
+    LED_correction = options['LED_correction'] # Correct kx,ky - LED coordinates
+    moderator_on = options['moderator_on'] # Increases stability
     
     # Other parameters
     img_size = images.shape[0] # Square, same size as pupil function
-    img_center = img_size // 2
     num_images = images.shape[2]
     obj_size = obj.shape[0] # Square
     obj_center = obj_size // 2 # Center of object (used for inserting spectra in correct place)
     pupil_binary = np.copy(pupil) # Original pupil function (binary mask)
     pupil = pupil.astype('complex64') # Pupil function for updating needs to be complex 
+
+    # Initialize empty lists to store good images and their coordinates
+    good_images = []
+    img_quality = []
+    kx_new = []
+    ky_new = []
+    
+    # Loop through each image and check if it meets the quality threshold
+    for i in range(num_images):
+        img = images[:, :, i] # Image to check
+        dynamic_range = (np.max(img) - np.min(img)) * 256
+        if dynamic_range >= quality_threshold:
+            good_images.append(img)   # Append the good image to the list
+            img_quality.append(dynamic_range) # Save the quality metric
+            kx_new.append(kx[i])      # Append the corresponding kx coordinate
+            ky_new.append(ky[i])      # Append the corresponding ky coordinate
+            
+    # Convert lists to numpy arrays and override the old variables to save memory
+    images = np.array(good_images)  # Shape will be (num_good_images, img_size, img_size)
+    images = np.transpose(good_images, (1, 2, 0)) # Shape will be (img_size, img_size, num_good_images) as required
+    num_images = images.shape[2]
+    img_quality = np.array(img_quality)
+    kx = np.array(kx_new)
+    ky = np.array(ky_new)
         
+    update_size = np.zeros(num_images) # To monitor object update size (can spot instability numerically)
+     
     # If plotting, create axis and figure here to save resources
     if plot_mode != 0:
         global pupil_radius 
-        fig, axes = plt.subplots(1, 4, figsize=(15,5))
-        plt.ion()  # Turn on interactive mode
+        fig, axes = plt.subplots(1, 3, figsize=(15,4))
 
+    # Main loop
     for iter in range(max_iter):
         for i in range(num_images): # For each image in data set   
-            x_start = int(obj_center + kx[i] - img_size//2) # For cropping object spectrum (see plots)
+            x_start = int(obj_center + kx[i] - img_size//2) # For cropping object spectrum
             y_start = int(obj_center - ky[i] - img_size//2)  
             
             # Define variables for object and pupil updating 
-              
+             
             # The relevant part of object spectrum to update
             object_update = obj[y_start:y_start+img_size, x_start:x_start+img_size]
              
@@ -186,34 +270,43 @@ def reconstruct(images, kx, ky, obj, pupil, options):
             # Object update
             numerator = np.abs(pupil) * np.conj(pupil) * update_image
             denominator = np.max(np.abs(pupil)) * (np.abs(pupil)**2 + alpha)
-            object_update = numerator / denominator   
-            obj[y_start:y_start+img_size, x_start:x_start+img_size] += 0.5*object_update # Add to main spectrum
-               
+            object_update = numerator / denominator
+            if moderator_on:
+                moderator = (img_quality[i]/np.max(img_quality)) # Moderate update step based on image quality (provides stability)
+            else:
+                moderator = 1
+            obj[y_start:y_start+img_size, x_start:x_start+img_size] += moderator * object_update # Add to main spectrum
+            update_size[i] = np.mean(np.abs(object_update)) # To check instability 
+            
             # Pupil update
             numerator = np.abs(object_update) * np.conj(object_update) * update_image * pupil_binary
             denominator = np.max(obj) * (np.abs(object_update)**2 + beta)
             pupil_update = numerator / denominator
-            pupil += 0.1*pupil_update
+            pupil += pupil_update
       
-            # print(np.mean(np.abs(object_update)))  
- 
+            # LED position (kx,ky) correction for image we just used
+            if LED_correction == 1:
+                kx_new,ky_new = update_LED_positions_accurate(obj,img,kx[i],ky[i],img_size,obj_center,i)
+                kx[i] = kx_new # Updated LED positions
+                ky[i] = ky_new
+                
             # Plot every image
             if plot_mode == 1:
-                plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter)
+                plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter,plot_mode,update_size)
         
         # Plot every iteration
         if plot_mode == 2:
-            plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter)
-            
-    # To keep the figure open when done plotting
+            plot(axes,fig, obj,x_start,y_start,img_size,obj_center,pupil,kx,ky,i,iter,plot_mode,update_size)
+    
+    # To keep plot open when function is done
     if plot_mode != 0:
-        plt.ioff()  # Turn off interactive mode when done
-        plt.show()  # Show the final figure if needed
+        plt.ioff()
+        plt.show()
 
-    return IFT(obj),pupil
+    return IFT(obj),pupil,kx,ky
 
 ###########################################################################################################################
-# Main script. Handles both image acquisition and reconstruction
+# Main script. Handles image acquisition and reconstruction and UI
 ###########################################################################################################################
 
 # Turn on RPI fan
@@ -230,26 +323,67 @@ line.set_value(0)  # 0 for LOW, 1 for HIGH
 # Release the line when done
 line.release()
 
-# Capture data...
+#######################################################################################################################
+# Image gathering
 
-# Load image data into an array
-data_path = 'data/talia_old'
-grid_size = 11 # 1 -> 16, determines how many images to reconstruct with, beyond 12x12 grid images are mostly useless
-img_size = 100 # Size of low res image (original or crop)
+# Set up image gathering stage
+grid_size = 5 # 1->16, recommend 4-8 for stability, time and performance balacnce
+num_images = grid_size**2 # Total number of FPM images
+brightfield_exposure = 50000  # In microseconds for brightfield
+fpm_exposure = 300000  # In microseconds for FPM
+img_size = 300 # 100-300 is sensible for square images (otherwise reconstruction will be too slow)
+x_coords,y_coords = LED_spiral(grid_size) # Generates arrays for x and y to turn on LEDs in spiral pattern
 
-num_images = grid_size**2
-images = np.zeros((img_size,img_size,num_images)) # Initialise array for storing images
-brightfield = np.array(Image.open(os.path.join(data_path,'brightfield.png')))/256 # Brightfield image for reference
-brightfield = brightfield[0:img_size,0:img_size] # Crop
+# Initialise LED array
+led_matrix = RPiLedMatrix()
+led_matrix.set_rotation(135) # Ensure 0,0 is bottom left pixel
 
-for i in range(num_images): # ~ 2s to load all 256 images into array
-    filename = os.path.join(data_path, f'image_{i}.png') # Construct path
-    img = np.array(Image.open(filename))/256 # Open image and convert to array, with data range 0-1
-    img = img[0:img_size,0:img_size] # Crop
-    images[:,:,i] = img
+# Initialize camera
+camera = Picamera2()
+still_config = camera.create_still_configuration(
+    main={'size': (img_size, img_size)}, controls={"AnalogueGain": 1}
+)
+camera.configure(still_config)
+
+# Show preview for alignment
+camera.start_preview()
+camera.start()
+print("Align your sample. Press Enter when ready.")
+input()  # Wait for user input to proceed
+camera.stop_preview()
+
+# Take a brightfield image
+camera.set_controls({"ExposureTime": brightfield_exposure})
+led_matrix.show_circle(radius=2, color='white', brightness=1)
+time.sleep(1)  # Allow LED to stabilize
+brightfield = camera.capture_array() # img_size x img_size RGB image
+brightfield = np.array(Image.fromarray(brightfield).convert('L')) # Convert to grayscale 
+
+# Take FPM images
+images = []  # List to store grayscale arrays
+camera.set_controls({"ExposureTime": fpm_exposure})
+
+for i in range(num_images):
+    led_matrix.show_pixel(x_coords[i], y_coords[i], brightness=1)
+    time.sleep(0.1)  # Short pause for LED
+    image = camera.capture_array() # img_size x img_size RGB image
+    image = np.array(Image.fromarray(image).convert('L'))  # Convert to grayscale
+    images.append(image)
+    
+# # Optional: Save brightfield and FPM images to output folder for debugging
+# output_folder = 'data/recent_data'
+# np.save(os.path.join(output_folder, 'brightfield.npy'), brightfield)
+# np.save(os.path.join(output_folder, 'fpm_images.npy'), np.array(images))
+
+# Turn off LED matrix and camera             
+led_matrix.off()
+camera.stop()
+camera.close()
+
+####################################################################################################################
+# Reconstruction
 
 # Experiment parameters
-x_coords, y_coords = LED_spiral(grid_size) # LED coordinates in sequence
 LED2SAMPLE = 54 # Distance from LED array to the sample, 54mm (larger distance leads to closer overlapping Fourier circles, optimal 40-60%)
 LED_P = 3.3 # LED pitch, mm
 N_GLASS = 1.52 # Glass refractive index
@@ -266,9 +400,6 @@ y_abs = (y_coords - y_coords[0])*LED_P # y distances of LEDs from center LED
 # Size of object image (for given parameters upsampling is between 2 and 5 depending on grid_size)
 # Can do seperately x and y if image is not square
 obj_size = calculate_object_size(img_size, grid_size, LED2SAMPLE, LED_P, NA, WLENGTH, PIX_SIZE)
-
-# Locations of LEDs in Fourier domain
-kx,ky = calculate_fourier_positions(x_abs, y_abs, LED2SAMPLE, WLENGTH, PIX_SIZE, img_size)
 
 # Initial pupil function (binary mask)
 # Nyquist sampling criterion: sampling_ratio >2 -> oversampling, sampling_ratio <2 -> undersampling (aliasing may occur)
@@ -289,11 +420,56 @@ obj = np.pad(F_img,pad_width,'constant',constant_values=0) # Initial object in 
 
 # Set parameters for reconstruction algorithm
 options = {
-    'max_iter': 10, # Number of iterations
-    'alpha': 1, # Regularisation parameter, <10
-    'beta': 100, # Regularisation parameter, >0
+    'max_iter': 8, # Number of iterations
+    'alpha': 6, # Regularisation parameter, <10, DOES make a difference, 5 seems good for most cases
+    'beta': 1, # Regularisation parameter, >0, not important
     'plot_mode': 2, # 0, off; 1, plot every image; 2, plot every iteration
+    'quality_threshold': 0, # Will only use images with dynamic range greater than this (set to 0 to use all images)
+    'moderator_on': False, # Will reduce impact on object update from low information images (helps stability)
+    'LED_correction': 0, # 0, off; 1, accurate; 2, fast (not working)
 }
 
+# Reconstruction with calculated kx and ky (quickstart)
+kx,ky = calculate_fourier_positions(x_abs, y_abs, LED2SAMPLE, WLENGTH, PIX_SIZE, img_size)
+rec_obj,rec_pupil,_,_ = reconstruct(images, kx, ky, obj, pupil, options)
 
-rec_obj, rec_pupil = reconstruct(images, kx, ky, obj, pupil, options)
+# # Reconstruction with LED correction to find optimal kx and ky
+# options = {
+#     'max_iter': 1, # Number of iterations
+#     'alpha': 5, # Regularisation parameter, <10, DOES make a difference, 5 seems good for most cases
+#     'beta': 1, # Regularisation parameter, >0, not important
+#     'plot_mode': 0, # 0, off; 1, plot every image; 2, plot every iteration
+#     'quality_threshold': 0, # Will only use images with dynamic range greater than this (set to 0 to use all images)
+#     'moderator_on': False, # Will reduce impact on object update from low information images (helps stability)
+#     'LED_correction': 1, # 0, off; 1, accurate; 2, fast (not working)
+# }
+# kx,ky = calculate_fourier_positions(x_abs, y_abs, LED2SAMPLE, WLENGTH, PIX_SIZE, img_size) # Initial guess for kx,ky
+# rec_obj,rec_pupil,kx_updated,ky_updated = reconstruct(images, kx, ky, obj, pupil, options)
+# np.save(os.path.join(data_path,'kx_updated'),kx_updated)
+# np.save(os.path.join(data_path,'ky_updated'),ky_updated)
+
+
+# # Reconstruction with loaded / optimal kx and ky (optimal values depend on crop size)
+# kx = np.load(os.path.join(data_path,'kx_updated.npy'))
+# ky = np.load(os.path.join(data_path,'ky_updated.npy'))
+# rec_obj,rec_pupil,_,_ = reconstruct(images, kx, ky, obj, pupil, options)
+
+# Plot results
+fig, axes = plt.subplots(1, 3, figsize=(15,4))
+
+obj_mag = np.abs(rec_obj)**2 # Magnitude - can increase contrast if necessary
+obj_arg = np.angle(rec_obj) # Phase
+
+axes[0].imshow(obj_mag,cmap='gray')
+axes[0].set_title('Object magnitude')
+axes[1].imshow(obj_arg, cmap='gray')
+axes[1].set_title('Object phase')
+axes[2].imshow(brightfield, cmap='gray')
+axes[2].set_title('Brightfield image')
+
+plt.show()
+
+# Save results
+obj_mag = (obj_mag / obj_mag.max() * 255).astype(np.uint8)
+obj_mag = Image.fromarray(obj_mag)
+obj_mag.save('results/magnitude.png')
